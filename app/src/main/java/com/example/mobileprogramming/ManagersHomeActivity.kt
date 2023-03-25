f794aa4eab8fd6ce76f0b5dc404701f933fef0b7
AlessandroS01 <seghini53@gmail.com>
2023-03-25 12:41:14 +0100

    Prova passaggio di parametri tra le varie view

--- app/src/main/java/com/example/mobileprogramming/ManagersHomeActivity.kt
+++ app/src/main/java/com/example/mobileprogramming/ManagersHomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mobileprogramming
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ManagersHomeActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class ManagersHomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_managers_home)
 
+        val matriculation = intent.getIntExtra("EXTRA_MATRICULATION" , 0)
+        val password = intent.getStringExtra("EXTRA_PASSWORD")
+
+        val testo = findViewById<TextView>(R.id.textViewggggg)
+        testo.text = "$matriculation and $password"
     }
 }
