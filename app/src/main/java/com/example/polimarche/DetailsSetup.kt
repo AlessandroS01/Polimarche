3a5e65afe43465ea09ecf6d9b4943c79e952580e
AlessandroS01 <seghini53@gmail.com>
2023-04-11 10:54:39 +0200

    Creato layout per visualizzare tutti i dettagli delle componenti di un singolo setup

--- app/src/main/java/com/example/polimarche/DetailsSetup.kt
+++ app/src/main/java/com/example/polimarche/DetailsSetup.kt
@@ -1,26 +1,99 @@
 package com.example.polimarche
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mobileprogramming.R
+import com.example.mobileprogramming.databinding.ActivityDetailsSetupBinding
+import com.example.mobileprogramming.databinding.FragmentManagersMainHomeBinding
 
 class DetailsSetup: AppCompatActivity() {
 
+    private lateinit var binding : ActivityDetailsSetupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details_setup)
+        binding = ActivityDetailsSetupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
         window.statusBarColor = Color.TRANSPARENT
         window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
 
 
-        val backButton : ImageButton = findViewById<ImageButton>(R.id.backButtonDetailSetup)
+        val backButton : ImageButton = binding.backButtonDetailSetup
         backButton.setOnClickListener {
             finish()
+        }
+
+        /*
+        Let the user navigate from the details of a setup to
+        the particular frame listing all the details of the single
+        wheel.
+         */
+        val frontRightWheel: ImageView = binding.frontRightWheelDetails
+        val frontLeftWheel: ImageView = binding.frontLeftWheelDetails
+        val rearRightWheel: ImageView = binding.rearRightWheelDetails
+        val rearLeftWheel: ImageView = binding.rearLeftWheelDetails
+        frontRightWheel.setOnClickListener {
+            Intent(this, DetailsWheel::class.java).apply {
+                startActivity(this)
+            }
+        }
+        frontLeftWheel.setOnClickListener {
+            Intent(this, DetailsWheel::class.java).apply {
+                startActivity(this)
+            }
+        }
+        rearRightWheel.setOnClickListener {
+            Intent(this, DetailsWheel::class.java).apply {
+                startActivity(this)
+            }
+        }
+        rearLeftWheel.setOnClickListener {
+            Intent(this, DetailsWheel::class.java).apply {
+                startActivity(this)
+            }
+        }
+
+        /*
+        Let the user navigate from the details of a setup to
+        the particular frame listing all the details of the single
+        damper.
+         */
+        val frontDamper: ImageView = binding.frontDamperDetails
+        val backDamper: ImageView = binding.backDamperDetails
+        frontDamper.setOnClickListener {
+            Intent(this, DetailsDamper::class.java).apply {
+                startActivity(this)
+            }
+        }
+        backDamper.setOnClickListener {
+            Intent(this, DetailsDamper::class.java).apply {
+                startActivity(this)
+            }
+        }
+
+        /*
+        Let the user navigate from the details of a setup to
+        the particular frame listing all the details of the single
+        spring.
+         */
+        val frontSpring: ImageView = binding.frontSpringDetails
+        val backSpring: ImageView = binding.backSpringDetails
+        frontSpring.setOnClickListener {
+            Intent(this, DetailsSpring::class.java).apply {
+                startActivity(this)
+            }
+        }
+        backSpring.setOnClickListener {
+            Intent(this, DetailsSpring::class.java).apply {
+                startActivity(this)
+            }
         }
     }
 
---
+++ app/src/main/java/com/example/polimarche/DetailsSpring.kt
@@ -0,0 +1,45 @@
+package com.example.polimarche
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobileprogramming.databinding.ActivityDetailsSpringBinding
+
+class DetailsSpring: AppCompatActivity() {
+
+    private lateinit var binding : ActivityDetailsSpringBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailsSpringBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+        window.statusBarColor = Color.TRANSPARENT
+        window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+
+
+        val backButton : ImageButton = binding.backButtonDetailSpring
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    /*
+        This method is used to set the status bar
+        completely transparent but keeping the icon at the top
+        of the layout
+     */
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
+    }
+}
