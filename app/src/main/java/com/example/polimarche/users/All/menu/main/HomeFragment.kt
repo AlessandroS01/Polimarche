c734d748566f7df7b17db386305dab1c92336e73
SimoneRecinelli <simonerecinelli@gmail.com>
2023-06-23 12:04:48 +0200

    problema con librerie

--- app/src/main/java/com/example/polimarche/users/All/menu/main/HomeFragment.kt
+++ app/src/main/java/com/example/polimarche/users/All/menu/main/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.example.polimarche.users.all.menu.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +12,15 @@ import com.example.polimarche.databinding.FragmentGeneralMainHomeBinding
 import com.example.polimarche.users.managers.menu.practice_session.ManagersPracticeSessionActivity
 import com.example.polimarche.users.managers.menu.setup.ManagersSetupActivity
 import com.example.polimarche.users.managers.menu.tracks.ManagersTracksActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class HomeFragment : Fragment(R.layout.fragment_general_main_home){
 
     private var _binding: FragmentGeneralMainHomeBinding? = null
     private val binding get() = _binding!!
+
+    private var matriculation: String = ""
 
 
     override fun onCreateView(
@@ -26,11 +31,29 @@ class HomeFragment : Fragment(R.layout.fragment_general_main_home){
 
         _binding = FragmentGeneralMainHomeBinding.inflate(inflater, container, false)
         return binding.root
+        var matriculation: String = ""
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Recupera l'ID dell'utente autenticato
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+
+        // Ottieni un'istanza del Firestore
+        val db = FirebaseFirestore.getInstance()
+
+        val usersCollection = FirebaseFirestore.getInstance().collection("users")
+        userId?.let { uid ->
+            usersCollection.document(uid).get().addOnSuccessListener { document ->
+                if (document.exists()) {
+                    val matricola = document.getString("matriculation")
+                    // Imposta la matricola nel TextView
+                    binding.textView24.text = matriculation
+                }
+            }
+        }
         /*
         Allows the user to navigate through the menu by clicking
         on the frame layout positioned in the Home.
@@ -50,6 +73,9 @@ class HomeFragment : Fragment(R.layout.fragment_general_main_home){
                 startActivity(this)
             }
         }
+
+        // Imposta il testo della matricola
+        binding.textView24.text = matriculation
     }
 
 }
